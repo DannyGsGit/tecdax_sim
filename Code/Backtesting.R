0f246d1ac43fe4289bdda8803f9d14a9f7fc9d41
Danny Godbout <danny.godbout@gmail.com>
2016-12-26 06:55:41 -0800

    Cleaned up stock index acquisition.

--- Code/Backtesting.R
+++ Code/Backtesting.R
@@ -2,15 +2,41 @@ library(quantmod)
 library(dplyr)
 
 
+
+
+
+
+#~~~~~~~~~~~~~~~~~~
+#### Functions ####
+#~~~~~~~~~~~~~~~~~~
+
+# Lookup stock histories and return a dataframe
+get_stock_history <- function(target.symbol) {
+  constituent <- as.data.frame(getSymbols(Symbols = target.symbol, src = "google", env = NULL))
+  
+  constituent$Date <- row.names(constituent)
+  
+  constituent$Symbol <- target.symbol
+  
+  colnames(constituent) <- c("Open", "High", "Low", "Close", "Volume", "Date", "Symbol")
+  
+  return(constituent)
+}
+
+
+
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Get stock histories ####
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 tecdax.index <- read.csv("./Data/Tecdax.csv", stringsAsFactors = FALSE)
 
-for (i in 1:nrow(tecdax.index)) {
-  quantmod::getSymbols(tecdax.index$Symbol[i], src = 'google')
-}
+tecdax <- lapply(1:nrow(tecdax.index), function(x) get_stock_history(tecdax.index$Symbol[x]))
+tecdax <- as.data.frame(do.call(rbind, tecdax))
+
+
+
 
 
 
@@ -25,3 +51,6 @@ for (i in 1:nrow(tecdax.index)) {
 # chartSeries(to.weekly(`FRA:EVT`),up.col='green',dn.col='red')
 # addBBands()
 # addMACD()
+
+
+
