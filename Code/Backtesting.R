6e3ee7bca76d2737f98a973fa76f4dee3139979c
Unknown <danny.godbout@gmail.com>
2016-12-25 22:55:34 -0800

    Tecdax constituent acquisition

--- Code/Backtesting.R
+++ Code/Backtesting.R
@@ -1,18 +1,27 @@
 library(quantmod)
 library(dplyr)
 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Get stock histories ####
-getSymbols('FRA:EVT', src = 'google')
-getSymbols('FRA:ADV', src = 'google')
-getSymbols('FRA:BC8', src = 'google')
-getSymbols('FRA:COK', src = 'google')
-getSymbols('FRA:AFX', src = 'google')
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+tecdax.index <- read.csv("./Data/Tecdax.csv", stringsAsFactors = FALSE)
+
+for (i in 1:nrow(tecdax.index)) {
+  quantmod::getSymbols(tecdax.index$Symbol[i], src = 'google')
+}
+
+
+
+#~~~~~~~~~~~~~~
 #### Chart ####
-chartSeries(`FRA:EVT`)
-barChart(`FRA:EVT`)
-candleChart(`FRA:EVT`, multi.col = TRUE, theme = "white")
+#~~~~~~~~~~~~~~
 
-chartSeries(to.weekly(`FRA:EVT`),up.col='green',dn.col='red')
-addBBands()
-addMACD()
+# chartSeries(`COPMX,A:GER`)
+# barChart(`FRA:EVT`)
+# candleChart(`FRA:EVT`, multi.col = TRUE, theme = "white")
+# 
+# chartSeries(to.weekly(`FRA:EVT`),up.col='green',dn.col='red')
+# addBBands()
+# addMACD()
